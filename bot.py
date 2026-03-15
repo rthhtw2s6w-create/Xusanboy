7008ac3811e141106d98059aa4d60056587b7533
rthhtw2s6w-create <rthhtw2s6w@privaterelay.appleid.com>
2026-03-15 17:09:52 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 import os
+import time
 import requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 def get_me():
-    url = f"{BASE_URL}/getMe"
-    response = requests.get(url)
-    print(response.text)
+    r = requests.get(f"{BASE_URL}/getMe")
+    print(r.text, flush=True)
+
+if not BOT_TOKEN:
+    print("BOT_TOKEN topilmadi")
+    exit()
 
 get_me()
+
+while True:
+    print("Bot ishlayapti...", flush=True)
+    time.sleep(60)
